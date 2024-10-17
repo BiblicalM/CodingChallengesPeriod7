35f690722264a379dbaecdf6484c1288f8223b0c
Maximiliano Gordillo <724910@student.dusd.net>
2024-10-17 14:54:22 -0700

    added project
    added my coding challenge project.

---
+++ CodingChallengesGordilloP7/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+
+
+class Challenges
+{
+
+
+
+    public static void Main(string[] args)
+    {
+        
+        //Welcome the user to my program
+        Console.WriteLine("Welcome to my coding challenge program. I will be making a variety of functions for you to use.\nPlease pick one");
+        MenuSelect();
+        
+        ////////////////////////////////////////////////
+        
+        
+        
+
+        
+
+        
+
+        
+
+
+    }
+
+    public static void MenuSelect()
+    {
+        Console.Clear();
+        Console.WriteLine("Please select a function by entering the number corresponding to the fuction.\n1. Sum\n2. Minutes To Seconds\n3. Plus One\n4. Circuit Power\n5. Years To Days\n6. Area of a Triangle");
+        Console.WriteLine("7. Less than or equal to Zero\n");
+        int selection = int.Parse(Console.ReadLine());
+        Console.Clear();
+        if (selection == 1)
+        {
+            //Challenge 1 Number Adder
+            Console.WriteLine("We are going to use the number adder today called sum. Please give me two numbers");
+
+            var number1 = Console.ReadLine();
+            int numberToUse1 = int.Parse(number1);
+
+            Console.WriteLine("Got it! Give me your next number!\n");
+            var number2 = Console.ReadLine();
+            int numberToUse2 = int.Parse(number2);
+
+            Console.WriteLine("The sum of the number: " + number1 + " and the number: " + number2 + " is equal to " + Sum(numberToUse1, numberToUse2));
+        }
+        else if (selection == 2)
+        {
+            //cHALLENGE 2 MInutes to seconds
+            Console.WriteLine("\nNext we are using the minutes to seconds converter, please enter a number for the minutes value\n");
+            float minutesToUse = float.Parse(Console.ReadLine());
+            Console.WriteLine(minutesToUse + " minute(s) is equal to " + MinutesToSeconds(minutesToUse) + " seconds\n");
+        }
+        else if (selection == 3)
+        {
+            //Challenge 3 Plus One
+            Console.WriteLine("Next we are going to use the PlusOne function, please enter a number\n");
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number3 + " plus 1 is equal to " + PlusOne(number3) + "\n");
+        }
+        else if (selection == 4)
+        {
+            //Challenge 4 Circuit Power
+            Console.WriteLine("Next we are going to use the CircutPower function, please enter numerical values for voltage and current\n");
+            int voltage = int.Parse(Console.ReadLine());
+            Console.WriteLine("Got it! Now enter the value for the current");
+            int current = int.Parse(Console.ReadLine());
+            Console.WriteLine("A voltage of " + voltage + " and a current of " + current + " equals a power of " + CircuitPower(voltage, current) + "\n");
+        }
+        else if (selection == 5)
+        {
+            //Challenge 5 years to days
+            Console.WriteLine("We are going to use that converts years to age, please enter a number for years\n");
+            int years = int.Parse(Console.ReadLine());
+            Console.WriteLine(years + " years is equal to " + YearsToDays(years) + " days\n");
+        }
+        else if (selection == 6)
+        {
+            //Challenge 6 Area of a triangle
+            Console.WriteLine("We are going to use a function that takes the base and height of a triangle and returns the area.\nPlease enter a base for the triangle.\n");
+            double triBase = double.Parse(Console.ReadLine());
+            Console.WriteLine("Got it! Now please enter a height for the triangle.");
+            double triHeight = double.Parse(Console.ReadLine());
+            Console.WriteLine("The area of a triangle with a base of " + triBase + " and a height of " + triHeight + " results in an area of " + TriangleArea(triBase, triHeight) + "\n");
+        }
+        else if (selection == 7)
+        {
+            //Challenge 7
+            Console.WriteLine("We are going to use a function that takes a number and say if it is less than/equal to zero or not.\nPlease enter a number to see if it is less than/equal to zero or not.");
+            int zeroLess = int.Parse(Console.ReadLine());
+            Console.WriteLine("The statement: '" + zeroLess + " is less than or equal to 0' is " + lessThanOrEqualToZero(zeroLess) + "\n");
+        }
+
+        Console.WriteLine("\n\nPlease enter any input to go back to the Menu");
+        Console.ReadLine();
+        MenuSelect();
+    }
+
+    public static int Sum(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public static float MinutesToSeconds(float minutes)
+    {
+        return minutes * 60;
+    }
+
+    public static int PlusOne(int number3)
+    {
+        return number3 +=1;
+    }
+
+    public static int CircuitPower(int voltage, int current)
+    {
+        return voltage * current;
+    }
+
+    public static int YearsToDays(int years)
+    {
+        return years * 365;
+    }
+
+    public static double TriangleArea(double base_, double height)
+    {
+        return (base_ * height) / 2;
+    }
+
+    public static bool lessThanOrEqualToZero(int number)
+    {
+        return number <= 0;
+    }
+}
+
+
