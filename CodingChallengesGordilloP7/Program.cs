1769b4c25a983c5e2e6bb2b1ca5e94d6c5d28b04
Maximiliano Gordillo <724910@student.dusd.net>
2025-01-08 14:47:30 -0800

    challenge 22
    min and max (hey that's my name) of array

--- CodingChallengesGordilloP7/Program.cs
+++ CodingChallengesGordilloP7/Program.cs
@@ -35,7 +35,7 @@ class Challenges
         Console.Clear();
         Console.WriteLine("Please select a function by entering the number corresponding to the fuction.\n1. Sum\n2. Minutes To Seconds\n3. Plus One\n4. Circuit Power\n5. Years To Days\n6. Area of a Triangle");
         Console.WriteLine("7. Less than or equal to Zero\n8. Less than 100\n9. Is equal to\n10. Something\n13. Sum of Internal Angles\n14. Edabit\n15. True&&\n16. Basketball");
-        Console.WriteLine("17. Find Perimeter\n18. Hello User\n19. Animals\n20. Football Points\n21. Months");
+        Console.WriteLine("17. Find Perimeter\n18. Hello User\n19. Animals\n20. Football Points\n21. Months\n22. Array Sort");
         var selection = Console.ReadLine();
         Console.Clear();
         if (selection == "1")
@@ -218,6 +218,20 @@ class Challenges
             Console.WriteLine("We are using a function that takes a number between 1 through 12 and outputs the corresponding month of the year");
             Months();
             
+        }
+        else if (selection == "22")
+        {
+            //Challenge 22
+            Console.WriteLine("We are going to use a program that takes an array of numbers and return both the minimum and maximum numbers, in that order.\nPlease enter the number of items you want in your array.");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] arrayNumbers = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine("Please enter an integer for number " + (i+1) + " of the list");
+                arrayNumbers[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arrayNumbers);
+            Console.WriteLine("The minimun and maximum of the array is [" + arrayNumbers[0] + ", " + arrayNumbers[arrayNumbers.Length-1] + "]");
         }
         else if (selection == "do you hate humanity?")
         {
